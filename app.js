bf1724335471ab50f98630cc5337b692a6c99949
vwede-e <115898669+vwede-e@users.noreply.github.com>
2023-01-08 22:11:58 +0100

    added divide by 0 error and delete button functionality

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ function multiply(operand1, operand2) {
 }
 
 function divide(operand1, operand2) {
+    if (operand2 == 0) {
+        return 0;
+    }
     return typeof +operand1 === "number" && typeof +operand2 === "number" ?
     +(operand1) / +(operand2) : "Enter two valid numbers";
 }
@@ -35,92 +38,108 @@ function operate(operand1, operator, operand2) {
     }
 }
 
-calculatorValue = {
-    value1: "",
-    operator: "",
-    value2: "",
-    total: 0
+const calculatorValue = {value1: "", operator: "", value2: "", total: 0 };
+
+const input = document.querySelector(".input");
+const buttons = document.querySelectorAll("button");
+for (let button of buttons) {
+    button.addEventListener("click", (event) => {
+        if (event.target.classList.contains("number")) {
+            handleNumbers(event);
+        }
+
+        else if (event.target.classList.contains("operator")) {
+            handleOperators(event);
+        }
+
+        else if (event.target.classList.contains("dot")) {
+            addDot();
+        }
+
+        else if (event.target.classList.contains("reset")) {
+            resetCalc();
+        }
+
+        else if (event.target.classList.contains("equal")) {
+            equals();
+        }
+
+        else {
+            del();
+        }
+    })
 }
-
-input = document.querySelector("input");
-del = document.querySelector(".del");
-reset = document.querySelector(".reset");
-equal = document.querySelector(".equal");
-dot = document.querySelector(".dot");
-
-dot.addEventListener("click", addDot);
-
-
-function addDot(event) {
+function addDot() {
     if (!calculatorValue.operator && !calculatorValue.value1.includes(".")) {
         calculatorValue.value1 += ".";
-        input.value += ".";
+        input.textContent += ".";
         return;
     }
     if (calculatorValue.operator && !calculatorValue.value2.includes(".")) {
         calculatorValue.value2 += ".";
-        input.value += ".";
+        input.textContent += ".";
     }
 }
 
-numbers = document.querySelectorAll(".number");
-for (number of numbers) {
-    number.addEventListener("click", handleNumbers);
-}
-
-equal.addEventListener("click", equals);
-reset.addEventListener("click", resetCalc);
-del.addEventListener("click", ()=> input.value = input.value.slice(0,[input.value.length-1]));
-
-operators = document.querySelectorAll(".operator");
-for (operator of operators) {
-    operator.addEventListener("click", handleOperators)
+function del() {
+    input.textContent = input.textContent.slice(0, input.textContent.length -1); // delete display value
+    if (calculatorValue.value2) {
+        calculatorValue.value2 = calculatorValue.value2.slice(0, calculatorValue.value2.length-1); //delete from variable
+    }
+    else if(calculatorValue.operator) {
+        calculatorValue.operator = "";        
+    }
+    else {
+        calculatorValue.value1 = calculatorValue.value1.slice(0, calculatorValue.value1.length-1);
+    }
 }
 
 function handleNumbers(event) {
     if (!calculatorValue.operator) {
         calculatorValue.value1 += event.target.textContent;
-        input.value += event.target.textContent;
+        input.textContent += event.target.textContent;
     }
     else {
         calculatorValue.value2 += event.target.textContent;
-        input.value += event.target.textContent;
+        input.textContent += event.target.textContent;
     }
 }
 
 function handleOperators(event) {
     if (!calculatorValue.operator) { // if operator is pressed when no operation has been done.
         calculatorValue.operator = event.target.textContent;
-        input.value += calculatorValue.operator;
+        input.textContent += calculatorValue.operator;
     }
     else {
         if (calculatorValue.value2) { // when operator is pressed and two operands are already given
-            result = operate(calculatorValue.value1, calculatorValue.operator, calculatorValue.value2);
-            calculatorValue.total = +result;
+            let result = operate(calculatorValue.value1, calculatorValue.operator, calculatorValue.value2);
+            result = Number.isInteger(result) ? result : +result.toFixed(4);
+            calculatorValue.total += result;
             calculatorValue.value2 = "";
             calculatorValue.value1 = result;  // operation is done on the two operands and the total is
-            input.value = calculatorValue.value1; // given to the first value. second operand is made empty
+            input.textContent = calculatorValue.value1; // given to the first value. second operand is made empty
             calculatorValue.operator = event.target.textContent; // and the new operator replaces the old
-            input.value += calculatorValue.operator;          // while waiting for the second operand
+            input.textContent += calculatorValue.operator;          // while waiting for the second operand
         }                                                    // to be given.
 
         else {
             calculatorValue.operator = event.target.textContent; //  // corrects a mistakenly typed operator
-            input.value = input.value.slice(0, input.value.length-1) +  calculatorValue.operator;
+            input.textContent = input.textContent.slice(0, input.textContent.length-1) +  calculatorValue.operator;
         }
     
     } }
 
 function equals() {
-    result = operate(calculatorValue.value1, calculatorValue.operator, calculatorValue.value2);
+    let result = operate(calculatorValue.value1, calculatorValue.operator, calculatorValue.value2);
+    result = Number.isInteger(result) ? result : +result.toFixed(4);
     calculatorValue.total = result;
-    input.value = result;
+    input.textContent = result;
 }
 
 function resetCalc() {
-    input.value = "";
+    input.textContent = "";
     calculatorValue.value1 = "";
     calculatorValue.operator = "";
     calculatorValue.value2 = "";
-    calculatorValue.total = null;
+    calculatorValue.total = 0;
 }
