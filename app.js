599898e6879c38058614d928ff097c0a109da78a
vwede-e <115898669+vwede-e@users.noreply.github.com>
2023-01-09 21:25:43 +0100

    Fixed multiple logical errors such as divide by zero, reset function, and continous arithmetic function error

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // global variables
-const calculatorValue = {value1: "", operator: "", value2: "", total: 0 };
+const calculatorValue = {value1: "", operator: "", value2: "", total: null };
 const input = document.querySelector(".input");
 const buttons = document.querySelectorAll("button");
+input.textContent = "0";
 
 // event listeners
 for (let button of buttons) {
@@ -29,49 +30,29 @@ for (let button of buttons) {
         else {
             del();
         }
-    })
-}
+    }) }
 
 // functions
-function add(operand1, operand2) {
-    return typeof +operand1 === "number" && typeof +operand2 === "number" ?
-    +(operand1) + +(operand2) : "Enter two valid numbers";
-}
-
-function subtract(operand1, operand2) {
-    return typeof +operand1 === "number" && typeof +operand2 === "number" ?
-    +(operand1) - +(operand2) : "Enter two valid numbers";
-}
-
-function multiply(operand1, operand2) {
-    return typeof +operand1 === "number" && typeof +operand2 === "number" ?
-    +(operand1) * +(operand2) : "Enter two valid numbers";
-}
-
-function divide(operand1, operand2) {
-    if (operand2 == 0) {
-        return 0;
-    }
-    return typeof +operand1 === "number" && typeof +operand2 === "number" ?
-    +(operand1) / +(operand2) : "Enter two valid numbers";
-}
-
 function operate(operand1, operator, operand2) {
-    let result;
     switch (operator) {
         case "+":
-            return result = add(operand1, operand2);
+            return +operand1 + +operand2;
         case "-":
-           return result = subtract(operand1, operand2);
+           return +operand1 - +operand2;
         case "*":
-            return result = multiply(operand1, operand2);
+            return +operand1 * +operand2;
         case "/":
-            return result = divide(operand1, operand2);
+            if (+operand2 === 0) {
+                alert("ERROR! CAN'T DIVIDE BY ZERO");
+                return 0;}
+            return +operand1 / +operand2;
+            break;
+        case "":
+            return +operand1;
             break;
         default:
             return "Enter a valid operator";
-    }
-}
+    } }
 
 function addDot() {
     if (!calculatorValue.operator && !calculatorValue.value1.includes(".")) {
@@ -82,8 +63,7 @@ function addDot() {
     if (calculatorValue.operator && !calculatorValue.value2.includes(".")) {
         calculatorValue.value2 += ".";
         input.textContent += ".";
-    }
-}
+    } }
 
 function del() {
     input.textContent = input.textContent.slice(0, input.textContent.length -1); // delete display value
@@ -95,55 +75,66 @@ function del() {
     }
     else {
         calculatorValue.value1 = calculatorValue.value1.slice(0, calculatorValue.value1.length-1);
-    }
-}
+    } }
 
 function handleNumbers(event) {
-    if (!calculatorValue.operator) {
-        calculatorValue.value1 += event.target.textContent;
-        input.textContent += event.target.textContent;
+    if (calculatorValue.total !== null) { //reset calculator before starting new operation
+        resetCalc();    
     }
+
+    if (!calculatorValue.operator) { // if operator is absent
+        calculatorValue.value1 += event.target.textContent; 
+        if (input.textContent.length == 1 && input.textContent[0] == "0") { // check if output has only 0. If true, change output to inputed number
+            input.textContent = event.target.textContent;
+        }
+
+        else {
+        input.textContent +=  event.target.textContent; //if output has number other than 0, append the new input number to it
+    } }
+
     else {
-        calculatorValue.value2 += event.target.textContent;
+        calculatorValue.value2 += event.target.textContent; // if the operator is true, append number to the second operand
         input.textContent += event.target.textContent;
-    }
-}
+    } }
 
 function handleOperators(event) {
-    if (!calculatorValue.operator) { // if operator is pressed when no operation has been done.
-        calculatorValue.operator = event.target.textContent;
+    if (!calculatorValue.operator) { // if there's no existing operator
+        calculatorValue.operator = event.target.textContent; 
         input.textContent += calculatorValue.operator;
     }
     else {
-        if (calculatorValue.value2) { // when operator is pressed and two operands are already given
-            let result = operate(calculatorValue.value1, calculatorValue.operator, calculatorValue.value2);
-            result = Number.isInteger(result) ? result : +result.toFixed(4);
-            calculatorValue.total += result;
+        if (calculatorValue.total !== null) { // if operator already exists and total is not null
+            calculatorValue.value1 = calculatorValue.total.toString();// give total to operand1 and empty total
+            calculatorValue.total = null;
             calculatorValue.value2 = "";
-            calculatorValue.value1 = result;  // operation is done on the two operands and the total is
-            input.textContent = calculatorValue.value1; // given to the first value. second operand is made empty
-            calculatorValue.operator = event.target.textContent; // and the new operator replaces the old
-            input.textContent += calculatorValue.operator;          // while waiting for the second operand
-        }                                                    // to be given.
+            input.textContent = calculatorValue.value1 + event.target.textContent; // operator should change to new one
+            calculatorValue.operator = event.target.textContent; // new operator should reflect on output
+        }
 
-        else {
+        else if (calculatorValue.value2) { //if operator exists, total is null, and 2nd operand is not empty
+            const result = equals();
+            calculatorValue.value2 = ""; //if operator and 2nd operand is present and no ouput
+            calculatorValue.value1 = result.toString(); //carry out the operation, give total to operand 1
+            input.textContent = calculatorValue.total + event.target.textContent; //operator should change to new one
+            calculatorValue.operator = event.target.textContent; // and should reflect on output
+            calculatorValue.total = null;
+        }
+
+        else { // if only operator and first operand is given
             calculatorValue.operator = event.target.textContent; //  // corrects a mistakenly typed operator
             input.textContent = input.textContent.slice(0, input.textContent.length-1) +  calculatorValue.operator;
-        }
-    
-    } }
+        } } }
 
 function equals() {
     let result = operate(calculatorValue.value1, calculatorValue.operator, calculatorValue.value2);
     result = Number.isInteger(result) ? result : +result.toFixed(4);
     calculatorValue.total = result;
     input.textContent = result;
-}
+    return result; }
 
 function resetCalc() {
-    input.textContent = "";
+    input.textContent = "0";
     calculatorValue.value1 = "";
     calculatorValue.operator = "";
     calculatorValue.value2 = "";
-    calculatorValue.total = 0;
-}
+    calculatorValue.total = null; }
